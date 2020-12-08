5e89fd8a9197c402d1cf82c7d9aa2284816f0ef2
leehanhom <leehanhom@hotmail.com>
2020-12-08 14:08:13 +0800

    addLi

--- main.js
+++ main.js
@@ -1,15 +1,43 @@
 window.addEventListener('DOMContentLoaded', function () {
-  document.querySelector('ul').addEventListener('click', function () {
-    var itemli = document.querySelectorAll('li');
-    for (var i = 0; i < itemli.length; i++) {
-      itemli[i].onclick = function () {
+  var itemLi = document.querySelectorAll('li')
+  var itemSpan = document.querySelectorAll('span')
+  var itemUl = document.querySelectorAll('ul')
+  var addBtn = document.getElementById('addBtn')
+  var inputvalue = document.getElementById('input')
+
+  function addLi(obj) {
+    var newLi = document.createElement("li");
+    newLi.innerHTML = (obj + '<span class="close">x</span>')
+    document.querySelector('ul').appendChild(newLi);
+  }
+  document.querySelector('li').addEventListener('click', function () {
+    for (var i = 0; i < itemLi.length; i++) {
+      itemLi[i].onclick = function () {
         this.classList.toggle('checked')
-        console.log(this.className)
       }
     }
   })
+
+  // document.querySelector('span').addEventListener('click', function () {
+  //   for (var i = 0; i < itemSpan.length; i++) {
+  //     itemSpan[i].onclick = function () {
+  //       itemLi.remove()
+  //       console.log('zzz')
+  //     }
+  //   }
+  // })
+
+  addBtn.addEventListener('click', function () {
+    var str = ""
+    var submitValue = inputvalue.value
+    str = submitValue;
+    addLi(str)
+    console.log(str)
+  })
+
+
+
+
 })
-
-
 
 
