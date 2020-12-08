24d0de8357cf5b27a18661e8b6033beb3ad45731
leehanhom <leehanhom@hotmail.com>
2020-12-08 16:27:58 +0800

    deletedLi

--- main.js
+++ main.js
@@ -2,14 +2,16 @@ window.addEventListener('DOMContentLoaded', function () {
   var itemLi = document.querySelectorAll('li')
   var itemSpan = document.querySelectorAll('span')
   var itemUl = document.querySelectorAll('ul')
+  var span = document.querySelectorAll('.close')
   var addBtn = document.getElementById('addBtn')
   var inputvalue = document.getElementById('input')
 
   function addLi(obj) {
     var newLi = document.createElement("li");
     newLi.innerHTML = (obj + '<span class="close">x</span>')
-    document.querySelector('ul').appendChild(newLi);
+    document.querySelector('ul').appendChild(newLi)
   }
+
   document.querySelector('li').addEventListener('click', function () {
     for (var i = 0; i < itemLi.length; i++) {
       itemLi[i].onclick = function () {
@@ -18,26 +20,26 @@ window.addEventListener('DOMContentLoaded', function () {
     }
   })
 
-  // document.querySelector('span').addEventListener('click', function () {
-  //   for (var i = 0; i < itemSpan.length; i++) {
-  //     itemSpan[i].onclick = function () {
-  //       itemLi.remove()
-  //       console.log('zzz')
-  //     }
-  //   }
-  // })
+  document.querySelector('.close').addEventListener('click',
+    function () {
+      for (var i = 0; i < itemLi.length; i++) {
+        // if (itemLi[i]) {}
+        span[i].onclick = function () {
+          this.parentNode.remove()
+        }
+      }
+
+    })
+
+
 
   addBtn.addEventListener('click', function () {
-    var str = ""
-    var submitValue = inputvalue.value
-    str = submitValue;
+    var str = inputvalue.value
     addLi(str)
-    console.log(str)
+    inputvalue.value = ""
   })
 
 
 
 
 })
-
-
